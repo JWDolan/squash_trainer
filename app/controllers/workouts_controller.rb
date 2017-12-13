2057be3d26f20b615bc9cb537dc0cf5e056671d8
James Dolan <jwdolan820@gmail.com>
2017-12-13 22:24:23 +0000

    Added show workout route to WorkoutsController and link from workouts/index.erb to newly created workouts/show.erb

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,8 +1,8 @@
 class WorkoutsController < ApplicationController
 
       get '/workouts' do
-            @workouts = Workout.all
-            erb :'workouts/index'
+         @workouts = Workout.all
+         erb :'workouts/index'
       end
 
       get '/workouts/new' do
@@ -10,8 +10,13 @@ class WorkoutsController < ApplicationController
       end
 
       post '/workouts' do
-            @workout = Workout.create(title: params[:title])
-            redirect to "/workouts"
+         @workout = Workout.create(title: params[:title])
+         redirect to "/workouts"
+      end
+
+      get '/workouts/:id' do
+         @workout = Workout.find_by_id(params[:id])
+         erb :'workouts/show'
       end
 
 end
