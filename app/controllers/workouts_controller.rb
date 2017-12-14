8940fcd89bd6f21d39f0f843d02d8e4f1030b720
James Dolan <jwdolan820@gmail.com>
2017-12-14 02:04:13 +0000

    Added validation to Workout model and error messages to WorkoutsController

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,10 +6,14 @@ class WorkoutsController < ApplicationController
       end
 
       get '/workouts/new' do
+         @error_message = params[:error]
          erb :'workouts/new'
       end
 
       post '/workouts' do
+         unless Workout.valid_params?(params)
+            redirect "/workouts/#{@workout.id}/edit?error=invalid workout"
+         end
          @workout = Workout.create(title: params[:title])
          redirect to "/workouts"
       end
@@ -20,12 +24,16 @@ class WorkoutsController < ApplicationController
       end
 
       get '/workouts/:id/edit' do
+         @error_message = params[:error]
          @workout = Workout.find_by_id(params[:id])
          erb :'workouts/edit'
       end
 
       patch '/workouts/:id' do
          @workout = Workout.find_by_id(params[:id])
+         unless Workout.valid_params?(params)
+            redirect "/workouts/#{@workout.id}/edit?error=invalid workout"
+         end
          @workout.title = params[:title]
          @workout.save
          redirect to "/workouts/#{@workout.id}"
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,9 @@
 class Workout < ActiveRecord::Base
    belongs_to :user
    has_many :exercises
+
+   def self.valid_params?(params)
+      return !params[:title].empty?
+   end
+
 end
