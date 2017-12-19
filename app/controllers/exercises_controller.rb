c95bb2a811362f3eaa9ccb1056d1c1084dfcfef6
James Dolan <jwdolan820@gmail.com>
2017-12-19 22:03:49 +0000

    Added get and post edit routes to the ExercisesController

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -5,4 +5,20 @@ class ExercisesController < ApplicationController
      erb :'exercises/new'
    end
 
+   get "/exercises/:id/edit" do
+     @error_message = params[:error]
+     @exercise = Exercise.find(params[:id])
+     erb :'exercises/edit'
+   end
+
+   post "/exercises/:id" do
+     @exercise = Exercise.find(params[:id])
+     unless Exercise.valid_params?(params)
+       redirect "/exercises/#{@exercise.id}/edit?error=invalid exercise"
+     end
+     @exercise.update(params.select{|k|k=="name" || k=="reps" || k=="workout_id"})
+     redirect "/exercises/#{@exercise.id}"
+   end
+
+
 end
