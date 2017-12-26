34b48f254ed2c528ba2bf2a2c194face31947772
James Dolan <jwdolan820@gmail.com>
2017-12-26 21:57:09 +0000

    Edited ApplicationController to include sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < Sinatra::Base
 
    configure do
          set :views, "app/views"
+         enable :sessions
+         set :session_secret, 'squashtrainer'
    end
 
    get '/' do
@@ -11,7 +13,11 @@ class ApplicationController < Sinatra::Base
    end
 
    get '/registrations/signup' do
+      if !session[:user_id]
          erb :'registrations/signup'
+      else
+         redirect '/workouts'
+      end
    end
 
    post '/registrations' do
@@ -19,18 +25,24 @@ class ApplicationController < Sinatra::Base
             redirect to '/registrations/signup'
          else
             @user = User.create(username: params[:username], password: params[:password])
+            session[:user_id] = @user.id
             redirect '/'
          end
    end
 
    get '/sessions/login' do
+      @error_message = params[:error]
+      if !session[:user_id]
          erb :'sessions/login'
+      else
+         redirect '/workouts'
+      end
    end
 
    post '/sessions' do
          @user = User.find_by(username: params[:username])
          if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
+            session[:user_id] = user.id
             redirect to '/account'
          else
             redirect to '/registrations/signup'
@@ -49,7 +61,7 @@ class ApplicationController < Sinatra::Base
    helpers do
          def redirect_if_not_logged_in
             if !logged_in?
-               redirect '/sessions/login'
+               redirect '/sessions/login?error=You have to be logged in to do that'
             end
          end
 
